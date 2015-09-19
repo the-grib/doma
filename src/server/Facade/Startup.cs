afe62224475447c6da9e6579b084d82e1bcb109b
Evgeniy Gryaznov <evgeniy.gryaznov@gmail.com>
2015-09-19 16:47:17 +0600

    HelloWorld

---
+++ src/server/Facade/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System.Web.Http;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Extensions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Owin;
+using Todom.Facade;
+
+[assembly: OwinStartup(typeof (Startup))]
+
+namespace Todom.Facade
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            ConfigureWebApi(config);
+
+            app.UseCors(CorsOptions.AllowAll);
+            app.UseWebApi(config);
+            app.UseStageMarker(PipelineStage.MapHandler);
+        }
+
+        public static void ConfigureWebApi(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute("DefaultApi", "{controller}/{id}", new {id = RouteParameter.Optional});
+            config.Routes.MapHttpRoute("DefaultCatchall", "{*url}", new {controller = "Error", action = "404"});
+
+            config.Formatters.JsonFormatter.SerializerSettings = new JsonSerializerSettings
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                NullValueHandling = NullValueHandling.Ignore
+            };
+        }
+    }
+}
